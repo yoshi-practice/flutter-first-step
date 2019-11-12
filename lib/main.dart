a4ab7fdd4ab45351779b260f00f6808b713a6556
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-12 23:58:19 +0900

    Add some display (esp: UI sample)

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MyApp()
-);
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColor: Colors.yellow
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title:
+            const Text('はじめてのFlutter',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        body: Center(
+          child: Text(
+            'Hello, world!',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.yellow,
+              textBaseline: TextBaseline.alphabetic,
+              fontSize: 50,
+            ),
+          ),
+        )
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-       _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+void main() => runApp(
+  MyApp()
+);
